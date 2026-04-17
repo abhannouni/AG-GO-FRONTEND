df237dbcc3f5caa4ebcdc67bbd431985d1b19b69
abhannouni <abdeslam.hannouni@gmail.com>
2026-04-17 10:28:13 +0100

    fix: update BASE_URL to use API_BASE_URL environment variable

--- src/API/apiClient.js
+++ src/API/apiClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3002/api/v1';
+const BASE_URL = process.env.API_BASE_URL || 'http://localhost:3002/api/v1';
 
 const apiClient = axios.create({
     baseURL: BASE_URL,
